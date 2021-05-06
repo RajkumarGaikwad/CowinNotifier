2ac2cafd40dc57881756383270bc26999e8713cb
Rajkumar Gaikwad <rajkumar.gaikwad3@gmail.com>
2021-05-06 23:02:25 +0530

    Update cowinNotifer.js

--- cowinNotifer.js
+++ cowinNotifer.js
@@ -1,108 +1,78 @@
- let punePinCodes = [
- "411001",
- "744301",
- "411026",
- "413102",
- "411041",
- "411027",
- "411045",
- "411006"
- ];
-
- (function(pincodes) {
 
 
-   getAreaInfo = (pincode) => {
+ (function() {
 
-	   	getCenter = (center) => {
-	   		console.log(center.innerText);
-	   		console.log(pincode);
-			let description  = center.innerText || '';
-			return description.includes(pincode);
-		};
+
+   getAreaInfo = () => {
 	   
 	   let centerlist = [];
-
 	   let htmlcollection = document.getElementsByClassName('center-name-text');
 
 	   for (i = 0; i < htmlcollection.length; i++) {
   			centerlist.push(htmlcollection[i]);
 		}
 
-
-	   let center = centerlist.filter(getCenter) || [];
-	   
-	   return center;
+	   return centerlist;
    
    };
+
+   let showAlert = async function (vaccinationAvailPincodes) {
+	    				let message = 'Vaccination available for : ' ;
+				      	var beepsound = new Audio( 'https://www.soundjay.com/button/sounds/beep-01a.mp3'); 
+						await beepsound.play(); 
+				   		clearInterval(this.intervalid);
+				   		alert(message + vaccinationAvailPincodes.toString());
+  	}
+
 
 
    checkStatus = () => {
 
-   	let message = 'Vaccination available for : ' ;
-   	let vaccinationAvailPincodes = [];
-   	let vaccinationNotAvailPincodes = [];
+	   	
+	   	let vaccinationAvailPincodes = [];
+	   	let vaccinationNotAvailPincodes = [];
+		
+		let areas = getAreaInfo();
 
-   	for (var pincode of pincodes) {
+      	if(areas.length > 1){
 
-   			let area = getAreaInfo(pincode);
+      		for (var idx = 0; idx < areas.length; idx++) {
+      			var actualparent = areas[idx];
+      			var placstr = actualparent.innerText;
 
-	      	if(area.length > 1){
-		   		area = area[0];
-		   	} else{
-		   		console.log("Didn't find area");
-		   		vaccinationNotAvailPincodes.push(pincode);
-		   		continue;
-		   	}
+		       let parent = actualparent.parentElement.parentElement.parentElement;
+			   let allplaces = parent.getElementsByClassName('vaccine-box') || [];
 
-		   let parent = area.parentElement.parentElement.parentElement;
-		   let allplaces = parent.getElementsByClassName('vaccine-box') || [];
+			   let allplacesarr = [];
 
-		   let allplacesarr = [];
-
-		   for (var i = 0; i < allplaces.length; i++) {
-	  			allplacesarr.push(allplaces[i]);
-			}
-	   
-		   for(var place of allplacesarr) {
-		      var placename = (place.firstChild.innerText || "").toLowerCase();
-		      if(placename != "booked"){
-		      	vaccinationAvailPincodes.push(pincode);
-		      	break;
-		      }
-	     }
-
-   	}
-
-   	if(vaccinationAvailPincodes.length > 1) {
-   		//clearInterval(this.intervalid);
-   		alert(message + vaccinationAvailPincodes.toString());
-   		window.open('/.');
-	 	
-   	}
-	//confirm(vaccinationNotAvailPincodes.toString());
-	console.log(vaccinationNotAvailPincodes);
-  
-   }
-
-   	//click search
+			   for (var i = 0; i < allplaces.length; i++) {
+		  			allplacesarr.push(allplaces[i]);
+				}
+		   
+			   for(var place of allplacesarr) {
+			      var placename = (place.firstChild.innerText || "").toLowerCase();
+			      if(!(placename == "booked" || placename == "na") ){
+			      	vaccinationAvailPincodes.push(placstr);
+			      	break;
+			      }
+		     }
+	 	} 
+	 }
+	 if (vaccinationAvailPincodes.length > 1) {
+	 	 showAlert(vaccinationAvailPincodes);
+	 }
+	
+   		//click search
 	let searchbtn = document.getElementsByClassName('pin-search-btn');
 	searchbtn[0].click();
 
 
-
-window.intervalid = setInterval(checkStatus, 5000);
-
-})(punePinCodes);
-
-  
-   
-  
-
-
-  
-  
-  
+  }
 
  
-  
+   
+
+
+window.intervalid = setInterval(checkStatus, 3000);
+
+})();
