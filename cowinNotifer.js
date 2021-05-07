0c705e6429a131df2ef9c82fef0e16401903eea1
Rajkumar Gaikwad <rajkumar.gaikwad3@gmail.com>
2021-05-07 20:26:04 +0530

    Update cowinNotifer.js

--- cowinNotifer.js
+++ cowinNotifer.js
@@ -1,109 +1,115 @@
- let punePinCodes = [
- "411001",
- "744301",
- "411026",
- "413102",
- "411041",
- "411027",
- "411045"
- ];
+let punePinCodes = [
+    "411001",
+    "744301",
+    "411026",
+    "413102",
+    "411041",
+    "411027",
+    "411045"
+];
 
-(function(pincodes) {
-
-
-   getAreaInfo = () => {
-	   
-	   let centerlist = [];
-	   let htmlcollection = document.getElementsByClassName('center-name-text');
-
-	   for (i = 0; i < htmlcollection.length; i++) {
-  			centerlist.push(htmlcollection[i]);
-		}
-
-	   return centerlist;
-   
-   };
-
-   getCenter = (areas, pincode) => {
-
-   		   	let selectedCenter = areas.filter(function(center){
-	   			console.log(center.innerText);
-		   		console.log(pincode);
-				let description  = center.innerText || '';
-				return description.includes(pincode);
-	   	}); 
-
-	   return selectedCenter;
-   };
-
-   getSelectedAreas = (areas = []) =>{
-
-   		let selectedCenters = [];
-
-   		for (var idx = 0; idx < pincodes.length; idx++) {
-	   		selectedCenters.push (getCenter(areas, pincodes[idx]));
-	   	}
-	   	return selectedCenters;
-   }
-
-   let showAlert = async function (vaccinationAvailPincodes) {
-	    				let message = 'Vaccination available for : ' ;
-				      	var beepsound = new Audio( 'https://www.soundjay.com/button/sounds/beep-01a.mp3'); 
-						await beepsound.play(); 
-				   		alert(message + vaccinationAvailPincodes);
-  	}
+function checkVaccineStatus() {
 
 
+    let showAlert = async function(vaccinationAvailPincodes) {
+        let message = 'Vaccination available for : ';
+        var beepsound = new Audio('https://www.soundjay.com/button/sounds/beep-01a.mp3');
+        await beepsound.play();
+        console.alert(message + vaccinationAvailPincodes);
+        console.info(message + vaccinationAvailPincodes);
+    }
 
-   checkStatus = () => {
-	   	
-	   	let vaccinationAvailPincodes = [];
-		
-		let areas = getAreaInfo();
 
-		let shortlistedAreas = getSelectedAreas(areas);
+    getAreaInfo = () => {
 
-  		for (var idx = 0; idx < shortlistedAreas.length; idx++) {
+        let centerlist = [];
+        let htmlcollection = document.getElementsByClassName('center-name-text');
 
-  			var shortlistedCenters = shortlistedAreas[idx] || [];
+        for (i = 0; i < htmlcollection.length; i++) {
+            centerlist.push(htmlcollection[i]);
+        }
 
-  			for (var i = 0; i < shortlistedCenters.length; i++) {
-  				
-	  			var actualparent = shortlistedCenters[idx];
+        return centerlist;
 
-	  			if (actualparent) {
-	  				var placstr = actualparent.innerText;
+    };
 
-			        let parent = actualparent.parentElement.parentElement.parentElement;
-				    let allplaces = parent.getElementsByClassName('vaccine-box') || [];
+    getCenter = (areas, pincode) => {
+        console.log(pincode);
 
-				    let allplacesarr = [];
+        let selectedCenter = areas.filter(function(center) {
+            console.log(center.innerText);
 
-				    for (var i = 0; i < allplaces.length; i++) {
-			  			allplacesarr.push(allplaces[i]);
-					}
 
-					for(var place of allplacesarr) {
-				      var placename = (place.firstChild.innerText || "").toLowerCase();
-				      if(!(placename == "booked" || placename == "na") ){
-				      	vaccinationAvailPincodes.push(placstr);
-				      	break;
-				      }
-		     		}
+            let description = center.innerText || '';
+            return description.includes(pincode);
+        });
 
-	  			}
-  			}
- 	} 
+        return selectedCenter;
+    };
 
-	 if (vaccinationAvailPincodes.length > 0) {
-	 	 showAlert(vaccinationAvailPincodes);
-	 }
-   		//click search
-	let searchbtn = document.getElementsByClassName('pin-search-btn');
-	searchbtn[0].click();
+    getSelectedAreas = (areas = []) => {
 
-  }
+        let selectedCenters = [];
 
-window.intervalid = setInterval(checkStatus, 10000);
+        for (var idx = 0; idx < punePinCodes.length; idx++) {
+            selectedCenters.push(getCenter(areas, punePinCodes[idx]));
+        }
+        return selectedCenters;
+    }
 
-})(punePinCodes);
+
+    checkStatus = () => {
+
+        let vaccinationAvailPincodes = [];
+
+        let areas = getAreaInfo();
+
+        let shortlistedAreas = getSelectedAreas(areas);
+
+        for (let idx = 0; idx < shortlistedAreas.length; idx++) {
+
+            var shortlistedCenters = shortlistedAreas[idx] || [];
+
+            for (let ci = 0; ci < shortlistedCenters.length; ci++) {
+
+                var actualparent = shortlistedCenters[ci];
+
+                if (actualparent) {
+                    var placstr = actualparent.innerText;
+
+                    let parent = actualparent.parentElement.parentElement.parentElement;
+                    let allplaces = parent.getElementsByClassName('vaccine-box') || [];
+
+                    let allplacesarr = [];
+
+                    for (let cdi = 0; cdi < allplaces.length; cdi++) {
+                        allplacesarr.push(allplaces[cdi]);
+                    }
+
+                    for (let place of allplacesarr) {
+                        var placename = (place.firstChild.innerText || "").toLowerCase();
+                        if (!(placename == "booked" || placename == "na")) {
+                            vaccinationAvailPincodes.push(placstr);
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        if (vaccinationAvailPincodes.length > 0) {
+            showAlert(vaccinationAvailPincodes);
+        }
+        //click search
+        let searchbtn = document.getElementsByClassName('pin-search-btn');
+        searchbtn[0].click();
+
+    }
+
+    checkStatus();
+
+
+}
+
+window.intervalid = setInterval(checkVaccineStatus, 5000);
